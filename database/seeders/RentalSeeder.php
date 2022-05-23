a0b401ab5aed3b9e36337b9a742c38beab938cb6
Aiki1111 <2022php42-student017@la-study.com>
2022-05-23 13:45:48 +0900

    Rental Seeder

--- database/seeders/RentalSeeder.php
+++ database/seeders/RentalSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use DateTime;
 
 class RentalSeeder extends Seeder
 {
@@ -16,22 +17,28 @@ class RentalSeeder extends Seeder
     {
         DB::table('rentals')->insert([
             [
-                'book_id'=>'001',
-                'member_id'=>'001',
-                'staff_id'=>'001',
+                'book_id'=>1,
+                'member_id'=>1,
+                'staff_id'=>1,
                 'returned_at'=>date('2020-01-01'),
+                'updated_at'=>new DateTime(),
+                'created_at'=>new DateTime(),
             ],
             [
-                'book_id'=>'002',
-                'member_id'=>'002',
-                'staff_id'=>'002',
+                'book_id'=>2,
+                'member_id'=>2,
+                'staff_id'=>2,
                 'returned_at'=>date('2020-01-02'),
+                'updated_at'=>new DateTime(),
+                'created_at'=>new DateTime(),
             ],
             [
-                'book_id'=>'003',
-                'member_id'=>'003',
-                'staff_id'=>'003',
+                'book_id'=>3,
+                'member_id'=>3,
+                'staff_id'=>3,
                 'returned_at'=>null,
+                'updated_at'=>new DateTime(),
+                'created_at'=>new DateTime(),
             ],
         ]);
     }
