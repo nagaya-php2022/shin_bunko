676b34f10e3a8e4f8035de4429dd85588fd29d0d
Aiki1111 <2022php42-student017@la-study.com>
2022-05-25 17:26:19 +0900

    members edit update

--- resources/views/members/data.blade.php
+++ resources/views/members/data.blade.php
@@ -1,4 +1,13 @@
 <dl class="memberData-container">
+    @if (isset($showEditBtn) && $showEditBtn)
+	<div class="show-editContainer">
+		<a class="orange-btn clickable btn" href="{{ route('members.edit', $member->id) }}">
+			編集する
+			<i class="fas fa-edit"></i>
+		</a>
+	</div>
+	@endif
+
     <dt>会員ID</dt>
     <dd>{{ $member->id ?: "-" }}</dd>
     <dt>電話番号</dt>
--- resources/views/members/show.blade.php
+++ resources/views/members/show.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.app')
 
+@section('style')
+<link rel="stylesheet" href="/css/data-container-style.css">
+<link rel="stylesheet" href="/css/show-common-style.css">
+@endsection
+
 @section('title')
 会員詳細
 @endsection
+
 @section('content')
+
 <h1>会員詳細</h1>
-@include('members.data')
+@include('members.data', ["showEditBtn" => true]) 
 @endsection
