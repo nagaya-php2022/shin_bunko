b6cc05a401f4052b966f4c76ad26d4a950466e47
U-Nuts <biafrajr@gmail.com>
2022-05-25 14:19:04 +0900

    set staff table as auth model for fortify _2

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -3,7 +3,6 @@
 namespace App\Actions\Fortify;
 
 use App\Models\Staff;
-use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
@@ -17,7 +16,7 @@ class CreateNewUser implements CreatesNewUsers
      * Validate and create a newly registered user.
      *
      * @param  array  $input
-     * @return \App\Models\User
+     * @return \App\Models\Staff
      */
     public function create(array $input)
     {
