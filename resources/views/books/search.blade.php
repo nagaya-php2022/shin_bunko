7a12ba7029edc63bf9b4a286527355642362c444
Aiki1111 <2022php42-student017@la-study.com>
2022-05-25 16:18:26 +0900

    book staff search.blade.php

--- resources/views/books/search.blade.php
+++ resources/views/books/search.blade.php
@@ -1,32 +1,41 @@
 @extends('layouts.app')
 
+@section("style")
+<link rel="stylesheet" href="/css/index-common-style.css">
+<link rel="stylesheet" href="/css/search-common-style.css">
+@endsection
+
 @section('content')
 <h1>資料検索</h1>
-<!--検索フォーム-->
-    <form action="{{ route('books.search') }}" method="get"><!--データの送信先の入力-->
-        <input type="number" name="book_id" id="book_id" placeholder="資料ID">
-        <button type="submit"><i class="fas fa-search"></i></button>
-    </form>
 
 <!--検索結果表示-->
-<table class="table"><!--テーブルのクラス名確認-->
-    <thead>
-        <tr>
-            <th>資料ID</th>
-            <th>タイトル</th>
-            <th>著者</th>
-            <th>出版社</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($books as $book) <!--booksテーブルのデータを引っ張ってくる-->
-            <tr>
-                <td>{{ $book->id }}</td>
-                <td><a href="{{ route('books.show', $book->id) }}">{{ $book->book_detail->name }}</a></td>
-                <td>{{ $book->book_detail->author }}</td>
-                <td>{{ $book->book_detail->publisher }}</td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="table-container">
+    <div class="index-searchContainer">
+        <!--検索フォーム-->
+        <form action="{{ route('books.search') }}" method="get"><!--データの送信先の入力-->
+            <input type="number" name="book_id" id="book_id" class="orange-input" placeholder="資料ID">
+            <button type="submit" class="clickable search-searchBtn"><i class="fas fa-search"></i></button>
+        </form>
+    </div>
+        <table class="table table-striped"><!--テーブルのクラス名確認-->
+            <thead>
+                <tr class="navy-bg">
+                    <th>資料ID</th>
+                    <th>タイトル</th>
+                    <th>著者</th>
+                    <th>出版社</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($books as $book) <!--booksテーブルのデータを引っ張ってくる-->
+                    <tr>
+                        <td>{{ $book->id }}</td>
+                        <td><a href="{{ route('books.show', $book->id) }}">{{ $book->book_detail->name }}</a></td>
+                        <td>{{ $book->book_detail->author }}</td>
+                        <td>{{ $book->book_detail->publisher }}</td>
+                    </tr>
+             @endforeach
+            </tbody>
+        </table>
+</div>
 @endsection
