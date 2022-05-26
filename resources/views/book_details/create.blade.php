a7cbaec2bbe2c9d7e406d4c961ff4a8472688c19
nagaya-php2022 <2022php42-student006@la-study.com>
2022-05-26 17:47:08 +0900

    change style

--- resources/views/book_details/create.blade.php
+++ resources/views/book_details/create.blade.php
@@ -3,31 +3,42 @@
 @section('title')
 ISBN登録
 @endsection
+
+@section('style')
+<link rel="stylesheet" href="/css/create-common-style.css">
+@endsection
+
 @section('content')
 
 <h1>ISBN登録</h1>
-@include('commons.flash')
-<form method="POST" action="{{route('book_details.store')}}">
-    @csrf
-    <label>ISBN番号<br>
-        <input type="number" name="isbn" value="{{ old('isbn') }}">
-    </label><br>
-    <label>資料名<br>
-        <input type="text" name="name" value="{{ old('name') }}">
-    </label><br>
-    <label>分類コード<br>
-        <input type="number" name="group_code" value="{{ old('group_code') }}">
-    </label><br>
-    <label>著者<br>
-        <input type="text" name="author" value="{{ old('author') }}">
-    </label><br>
-    <label>出版社<br>
-        <input type="text" name="publisher" value="{{ old('publisher') }}">
-    </label><br>
-    <label>出版日<br>
-        <input type="date" name="published_at" value="{{ old('published_at') }}">
-    </label><br>
 
-    <input type="submit" value="保存">
-</form>
+<div class="create-container">
+    <form method="POST" action="{{route('book_details.store')}}">
+        @csrf
+        @include('commons.flash')
+        <p><label>ISBN番号<span class="create-required-caption">必須</span><br>
+            <input type="number" name="isbn" value="{{ old('isbn') }}" placeholder="例）1111111111" class="orange-input">
+            <span class="create-description">（半角数字）</span>
+        </label></p>
+        <p><label>資料名<span class="create-required-caption">必須</span><br>
+            <input type="text" name="name" value="{{ old('name') }}" placeholder="例）シェイクスピア全集" class="orange-input">
+        </label></p>
+        <p><label>分類コード<span class="create-required-caption">必須</span><br>
+            <input type="number" name="group_code" value="{{ old('group_code') }}" placeholder="例）9" class="orange-input">
+            <span class="create-description">（半角数字）</span>
+        </label></p>
+        <p><label>著者<span class="create-required-caption">必須</span><br>
+            <input type="text" name="author" value="{{ old('author') }}" placeholder="例）福田恒存" class="orange-input">
+            <span class="create-description">（スペースなし）</span>
+        </label></p>
+        <p><label>出版社<span class="create-required-caption">必須</span><br>
+            <input type="text" name="publisher" value="{{ old('publisher') }}" placeholder="例）新潮社" class="orange-input">
+        </label></p>
+        <p><label>出版日<span class="create-required-caption">必須</span><br>
+            <input type="date" name="published_at" value="{{ old('published_at') }}" class="orange-input">
+        </label></p>
+        
+        <input type="submit" value="保存" class="orange-btn">
+    </form>
+</div>
 @endsection
