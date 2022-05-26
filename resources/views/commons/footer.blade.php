b932a64f8dde7bc380eb3ef5303c38b6a7d9d0c0
U-Nuts <biafrajr@gmail.com>
2022-05-26 09:27:44 +0900

    footer fixed

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -1,8 +1,12 @@
 <footer><footer>
 <style type = "text/css">
     footer{
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        left: 0;
         height:5em; /* フッターの幅 */
         background-color: #023c4c;
-        margin-top:5em;
+        /* margin-top:5em; */
     }
 </style>
